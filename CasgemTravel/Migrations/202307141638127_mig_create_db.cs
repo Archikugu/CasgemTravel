65f24658a4a662753066acc94b95d765bc24c4ad
Gokhan <engokhangok@gmail.com>
2023-07-14 19:48:43 +0300

    Hatalar Düzelti ve Database Oluşturuldu.

---
+++ CasgemTravel/Migrations/202307141638127_mig_create_db.cs
@@ -0,0 +1,100 @@
+﻿namespace CasgemTravel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_create_db : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        AdminID = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.AdminID);
+            
+            CreateTable(
+                "dbo.Bookings",
+                c => new
+                    {
+                        BookingID = c.Int(nullable: false, identity: true),
+                        CustomerName = c.String(),
+                        Destination = c.String(),
+                        Duration = c.String(),
+                        Mail = c.String(),
+                        BookingDate = c.DateTime(nullable: false),
+                        BookingStatus = c.String(),
+                    })
+                .PrimaryKey(t => t.BookingID);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ContactID = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        Mail = c.String(),
+                        Subject = c.String(),
+                        Message = c.String(),
+                        MessageDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ContactID);
+            
+            CreateTable(
+                "dbo.Destinations",
+                c => new
+                    {
+                        DestinationID = c.Int(nullable: false, identity: true),
+                        DestinationName = c.String(),
+                        DestinationDayNight = c.String(),
+                        Capacity = c.Byte(nullable: false),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ImageUrl = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.DestinationID);
+            
+            CreateTable(
+                "dbo.Guides",
+                c => new
+                    {
+                        GuideID = c.Int(nullable: false, identity: true),
+                        GuideName = c.String(),
+                        GuideTitle = c.String(),
+                        GuideImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.GuideID);
+            
+            CreateTable(
+                "dbo.SocialMedias",
+                c => new
+                    {
+                        SocialMediaID = c.Int(nullable: false, identity: true),
+                        SocialName = c.String(),
+                        SocialMediaUrl = c.String(),
+                        GuideID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.SocialMediaID)
+                .ForeignKey("dbo.Guides", t => t.GuideID, cascadeDelete: true)
+                .Index(t => t.GuideID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SocialMedias", "GuideID", "dbo.Guides");
+            DropIndex("dbo.SocialMedias", new[] { "GuideID" });
+            DropTable("dbo.SocialMedias");
+            DropTable("dbo.Guides");
+            DropTable("dbo.Destinations");
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Bookings");
+            DropTable("dbo.Admins");
+        }
+    }
+}
